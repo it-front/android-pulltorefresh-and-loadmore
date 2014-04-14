d4463383f1e64348882f3f02157edd35915c0ef2
Dong Weiqiang <weiqiang@teambition.com>
2014-04-15 02:36:51 +0800

    change bg to white

--- pulltorefresh-and-loadmore/src/com/costum/android/widget/LoadMoreListView.java
+++ pulltorefresh-and-loadmore/src/com/costum/android/widget/LoadMoreListView.java
@@ -1,18 +1,13 @@
 package com.costum.android.widget;
 
-import com.android.widget.R;
-
 import android.content.Context;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.AbsListView;
-import android.widget.ListAdapter;
-import android.widget.ListView;
+import android.widget.*;
 import android.widget.AbsListView.OnScrollListener;
-import android.widget.ProgressBar;
-import android.widget.RelativeLayout;
+import com.android.widget.R;
 
 /*
  * Copyright (C) 2012 Fabian Leon Ortega <http://orleonsoft.blogspot.com/,
@@ -33,160 +28,160 @@ import android.widget.RelativeLayout;
 
 public class LoadMoreListView extends ListView implements OnScrollListener {
 
-	private static final String TAG = "LoadMoreListView";
+    private static final String TAG = "LoadMoreListView";
 
-	/**
-	 * Listener that will receive notifications every time the list scrolls.
-	 */
-	private OnScrollListener mOnScrollListener;
-	private LayoutInflater mInflater;
+    /**
+     * Listener that will receive notifications every time the list scrolls.
+     */
+    private OnScrollListener mOnScrollListener;
+    private LayoutInflater mInflater;
 
-	// footer view
-	private RelativeLayout mFooterView;
-	// private TextView mLabLoadMore;
-	private ProgressBar mProgressBarLoadMore;
+    // footer view
+    private RelativeLayout mFooterView;
+    private TextView mLabLoadMore;
+    private ProgressBar mProgressBarLoadMore;
 
-	// Listener to process load more items when user reaches the end of the list
-	private OnLoadMoreListener mOnLoadMoreListener;
-	// To know if the list is loading more items
-	private boolean mIsLoadingMore = false;
-	private int mCurrentScrollState;
+    // Listener to process load more items when user reaches the end of the list
+    private OnLoadMoreListener mOnLoadMoreListener;
+    // To know if the list is loading more items
+    private boolean mIsLoadingMore = false;
+    private int mCurrentScrollState;
 
-	public LoadMoreListView(Context context) {
-		super(context);
-		init(context);
-	}
+    public LoadMoreListView(Context context) {
+        super(context);
+        init(context);
+    }
 
-	public LoadMoreListView(Context context, AttributeSet attrs) {
-		super(context, attrs);
-		init(context);
-	}
+    public LoadMoreListView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
 
-	public LoadMoreListView(Context context, AttributeSet attrs, int defStyle) {
-		super(context, attrs, defStyle);
-		init(context);
-	}
+    public LoadMoreListView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(context);
+    }
 
-	private void init(Context context) {
+    private void init(Context context) {
 
-		mInflater = (LayoutInflater) context
-				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        mInflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
-		// footer
-		mFooterView = (RelativeLayout) mInflater.inflate(
-				R.layout.load_more_footer, this, false);
-		/*
-		 * mLabLoadMore = (TextView) mFooterView
+        // footer
+        mFooterView = (RelativeLayout) mInflater.inflate(
+                R.layout.load_more_footer, this, false);
+        /*
+         * mLabLoadMore = (TextView) mFooterView
 		 * .findViewById(R.id.load_more_lab_view);
 		 */
-		mProgressBarLoadMore = (ProgressBar) mFooterView
-				.findViewById(R.id.load_more_progressBar);
 
-		addFooterView(mFooterView);
+        mLabLoadMore = (TextView) mFooterView.findViewById(R.id.load_more_lab_view);
+        mProgressBarLoadMore = (ProgressBar) mFooterView
+                .findViewById(R.id.load_more_progressBar);
 
-		super.setOnScrollListener(this);
-	}
+        addFooterView(mFooterView);
 
-	@Override
-	public void setAdapter(ListAdapter adapter) {
-		super.setAdapter(adapter);
-	}
+        super.setOnScrollListener(this);
+    }
 
-	/**
-	 * Set the listener that will receive notifications every time the list
-	 * scrolls.
-	 * 
-	 * @param l
-	 *            The scroll listener.
-	 */
-	@Override
-	public void setOnScrollListener(AbsListView.OnScrollListener l) {
-		mOnScrollListener = l;
-	}
+    @Override
+    public void setAdapter(ListAdapter adapter) {
+        super.setAdapter(adapter);
+    }
 
-	/**
-	 * Register a callback to be invoked when this list reaches the end (last
-	 * item be visible)
-	 * 
-	 * @param onLoadMoreListener
-	 *            The callback to run.
-	 */
+    /**
+     * Set the listener that will receive notifications every time the list
+     * scrolls.
+     *
+     * @param l The scroll listener.
+     */
+    @Override
+    public void setOnScrollListener(AbsListView.OnScrollListener l) {
+        mOnScrollListener = l;
+    }
 
-	public void setOnLoadMoreListener(OnLoadMoreListener onLoadMoreListener) {
-		mOnLoadMoreListener = onLoadMoreListener;
-	}
+    /**
+     * Register a callback to be invoked when this list reaches the end (last
+     * item be visible)
+     *
+     * @param onLoadMoreListener The callback to run.
+     */
 
-	public void onScroll(AbsListView view, int firstVisibleItem,
-			int visibleItemCount, int totalItemCount) {
+    public void setOnLoadMoreListener(OnLoadMoreListener onLoadMoreListener) {
+        mOnLoadMoreListener = onLoadMoreListener;
+    }
 
-		if (mOnScrollListener != null) {
-			mOnScrollListener.onScroll(view, firstVisibleItem,
-					visibleItemCount, totalItemCount);
-		}
+    public void onScroll(AbsListView view, int firstVisibleItem,
+                         int visibleItemCount, int totalItemCount) {
 
-		if (mOnLoadMoreListener != null) {
+        if (mOnScrollListener != null) {
+            mOnScrollListener.onScroll(view, firstVisibleItem,
+                    visibleItemCount, totalItemCount);
+        }
 
-			if (visibleItemCount == totalItemCount) {
-				mProgressBarLoadMore.setVisibility(View.GONE);
-				// mLabLoadMore.setVisibility(View.GONE);
-				return;
-			}
+        if (mOnLoadMoreListener != null) {
 
-			boolean loadMore = firstVisibleItem + visibleItemCount >= totalItemCount;
+            if (visibleItemCount == totalItemCount) {
+                mProgressBarLoadMore.setVisibility(View.GONE);
+                mLabLoadMore.setVisibility(View.GONE);
+                return;
+            }
 
-			if (!mIsLoadingMore && loadMore
-					&& mCurrentScrollState != SCROLL_STATE_IDLE) {
-				mProgressBarLoadMore.setVisibility(View.VISIBLE);
-				// mLabLoadMore.setVisibility(View.VISIBLE);
-				mIsLoadingMore = true;
-				onLoadMore();
-			}
+            boolean loadMore = firstVisibleItem + visibleItemCount >= totalItemCount;
 
-		}
+            if (!mIsLoadingMore && loadMore
+                    && mCurrentScrollState != SCROLL_STATE_IDLE) {
+                mProgressBarLoadMore.setVisibility(View.VISIBLE);
+                mLabLoadMore.setVisibility(View.VISIBLE);
+                mIsLoadingMore = true;
+                onLoadMore();
+            }
 
-	}
+        }
 
-	public void onScrollStateChanged(AbsListView view, int scrollState) {
-	
-		//bug fix: listview was not clickable after scroll
-		if ( scrollState == OnScrollListener.SCROLL_STATE_IDLE )
-        	{
-          		view.invalidateViews();
-        	}
-        	
-		mCurrentScrollState = scrollState;
+    }
 
-		if (mOnScrollListener != null) {
-			mOnScrollListener.onScrollStateChanged(view, scrollState);
-		}
+    public void onScrollStateChanged(AbsListView view, int scrollState) {
 
-	}
+        //bug fix: listview was not clickable after scroll
+        if (scrollState == OnScrollListener.SCROLL_STATE_IDLE) {
+            view.invalidateViews();
+        }
 
-	public void onLoadMore() {
-		Log.d(TAG, "onLoadMore");
-		if (mOnLoadMoreListener != null) {
-			mOnLoadMoreListener.onLoadMore();
-		}
-	}
+        mCurrentScrollState = scrollState;
 
-	/**
-	 * Notify the loading more operation has finished
-	 */
-	public void onLoadMoreComplete() {
-		mIsLoadingMore = false;
-		mProgressBarLoadMore.setVisibility(View.GONE);
-	}
+        if (mOnScrollListener != null) {
+            mOnScrollListener.onScrollStateChanged(view, scrollState);
+        }
 
-	/**
-	 * Interface definition for a callback to be invoked when list reaches the
-	 * last item (the user load more items in the list)
-	 */
-	public interface OnLoadMoreListener {
-		/**
-		 * Called when the list reaches the last item (the last item is visible
-		 * to the user)
-		 */
-		public void onLoadMore();
-	}
+    }
+
+    public void onLoadMore() {
+        Log.d(TAG, "onLoadMore");
+        if (mOnLoadMoreListener != null) {
+            mOnLoadMoreListener.onLoadMore();
+        }
+    }
+
+    /**
+     * Notify the loading more operation has finished
+     */
+    public void onLoadMoreComplete() {
+        mIsLoadingMore = false;
+        mProgressBarLoadMore.setVisibility(View.GONE);
+        mLabLoadMore.setVisibility(View.GONE);
+    }
+
+    /**
+     * Interface definition for a callback to be invoked when list reaches the
+     * last item (the user load more items in the list)
+     */
+    public interface OnLoadMoreListener {
+        /**
+         * Called when the list reaches the last item (the last item is visible
+         * to the user)
+         */
+        public void onLoadMore();
+    }
 
 }
